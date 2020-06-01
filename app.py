41b2e1ca0b16600478e9260cf738b02defb86d36
HishamAssam <40601614+HishamAssam@users.noreply.github.com>
2020-06-02 00:13:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 # Import required libraries
-import os
-from random import randint
-
-
 import flask
 import dash
 import dash_core_components as dcc
@@ -12,9 +8,7 @@ import dash_html_components as html
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+app = dash.Dash()
 
 app.layout = html.Div(
 html.H1(children="Hello Dash!")
