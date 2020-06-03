82420fd89efb26d26c525f291abd3c8439c74f1a
HishamAssam <40601614+HishamAssam@users.noreply.github.com>
2020-06-03 12:40:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-#import os
-#from random import randint
-
-
 import flask
 import dash
 import dash_core_components as dcc
@@ -11,9 +7,8 @@ from dash.dependencies import Input, Output
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-#server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+server = flask.Flask("main")
+app = dash.Dash("main", server=server)
 
 
 
@@ -36,5 +31,4 @@ def calc(val):
 
 
 # Run the Dash app
-if __name__ == '__main__':
-    app.server.run(debug=True, use_reloader=False)
+app.server.run(debug=True, use_reloader=False)
